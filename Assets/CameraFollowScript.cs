65348266f27754b8e1605abdc8112d601390c006
Oliver Stadie <hello@waog.net>
2016-03-23 04:57:16 +0100

    UPDATE smooth camera movement

---
+++ Assets/CameraFollowScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+
+	public Transform targetTransform;
+
+	[Tooltip("0 = camera doesn't move; 1 = camera is at target position instantly")]
+	public float drag = 0.2f;
+
+	void Update () {
+		transform.position = Vector3.Lerp (transform.position, targetTransform.position, drag);
+		Vector3 newEulerAngles = new Vector3 ();
+		newEulerAngles.x = Mathf.LerpAngle (transform.eulerAngles.x, targetTransform.eulerAngles.x, drag);
+		newEulerAngles.y = Mathf.LerpAngle (transform.eulerAngles.y, targetTransform.eulerAngles.y, drag);
+		newEulerAngles.z = Mathf.LerpAngle (transform.eulerAngles.z, targetTransform.eulerAngles.z, drag);
+		transform.eulerAngles = newEulerAngles;
+	}
+}

1850b4b6fce14daa621ed03ee1dfe30edd6842df
Oliver Stadie <hello@waog.net>
2016-03-28 21:43:54 +0200

    Clean up

--- Assets/MoveForwardScript.cs
+++ Assets/MoveForwardScript.cs
@@ -9,7 +9,7 @@ public class MoveForwardScript : MonoBehaviour {
 	private float[] playerPositions = { -1.9f, -0.95f, 0f, 0.95f, 1.9f };
 	private int curPlayerPositionIndex = 2;
 
-	private Transform transformToTurnOn = null;
+	private Transform enteredTurnSegment = null;
 
 	private bool waitingForHorizontalAxisInput = true;
 	
@@ -23,21 +23,10 @@ public class MoveForwardScript : MonoBehaviour {
 
 				Transform player = transform.GetChild (0);
 
-				if (transformToTurnOn != null) {
-					transform.position = new Vector3 (player.position.x, 0, player.position.z);
-					transform.RotateAround (transform.position, Vector3.up, Input.GetAxisRaw ("Horizontal") * 90);
-					Vector3 globalDiffVectorToCurveCenter = transformToTurnOn.position - transform.position;
-					Vector3 localDiffVectorToCurveCenter = transform.InverseTransformDirection (globalDiffVectorToCurveCenter);
-					Vector3 localDiffVectorToCurveCenterProjected = new Vector3 (localDiffVectorToCurveCenter.x, 0, 0);
-					Vector3 globalDiffVectorToCurveCenterProjected = transform.TransformDirection (localDiffVectorToCurveCenterProjected);
-					transform.position += globalDiffVectorToCurveCenterProjected;
-					transformToTurnOn = null;
-					curPlayerPositionIndex = 2;
-					player.localPosition = new Vector3 (playerPositions [curPlayerPositionIndex], 0.1f, 0);
+				if (enteredTurnSegment != null) {
+					turnAccordingToInput (player);
 				} else {
-					curPlayerPositionIndex += Mathf.RoundToInt(Input.GetAxisRaw ("Horizontal"));
-					curPlayerPositionIndex = Mathf.Clamp (curPlayerPositionIndex, 0, 4);
-					player.localPosition = new Vector3 (playerPositions [curPlayerPositionIndex], 0.1f, 0);
+					switchLaneAccordingToInput (player);
 				}
 			}
 		}
@@ -47,13 +36,36 @@ public class MoveForwardScript : MonoBehaviour {
 		}    
 	}
 
+	void turnAccordingToInput (Transform player)
+	{
+		transform.position = new Vector3 (player.position.x, 0, player.position.z);
+		transform.RotateAround (transform.position, Vector3.up, Input.GetAxisRaw ("Horizontal") * 90);
+		Vector3 globalDiffVectorToCurveCenter = enteredTurnSegment.position - transform.position;
+		Vector3 localDiffVectorToCurveCenter = transform.InverseTransformDirection (globalDiffVectorToCurveCenter);
+		Vector3 localDiffVectorToCurveCenterProjected = new Vector3 (localDiffVectorToCurveCenter.x, 0, 0);
+		Vector3 globalDiffVectorToCurveCenterProjected = transform.TransformDirection (localDiffVectorToCurveCenterProjected);
+		transform.position += globalDiffVectorToCurveCenterProjected;
+		enteredTurnSegment = null;
+		//					curPlayerPositionIndex = 2;
+		//					player.localPosition = new Vector3 (playerPositions [curPlayerPositionIndex], 0.1f, 0);
+	}
+
+	void switchLaneAccordingToInput (Transform player)
+	{
+		curPlayerPositionIndex += Mathf.RoundToInt (Input.GetAxisRaw ("Horizontal"));
+		curPlayerPositionIndex = Mathf.Clamp (curPlayerPositionIndex, 0, 4);
+		player.localPosition = new Vector3 (playerPositions [curPlayerPositionIndex], 0.1f, 0);
+	}
+
 	void OnTriggerEnter(Collider other) {
-		if (other.transform.name.Contains ("L-Segment") || other.transform.name.Contains ("T-Segment") || other.transform.name.Contains ("O-Segment")) {
-			transformToTurnOn = other.transform;
-			Debug.Log ("global: " + (transformToTurnOn.position - transform.position));
-			Debug.Log ("local direction: " + transform.InverseTransformDirection(transformToTurnOn.position - transform.position));
+		if (other.transform.name.Contains ("L-Segment") 
+				|| other.transform.name.Contains ("T-Segment") 
+				|| other.transform.name.Contains ("O-Segment")) {
+			enteredTurnSegment = other.transform;
+			Debug.Log ("global: " + (enteredTurnSegment.position - transform.position));
+			Debug.Log ("local direction: " + transform.InverseTransformDirection(enteredTurnSegment.position - transform.position));
 		} else {
-			transformToTurnOn = null;
+			enteredTurnSegment = null;
 		}
 	}
 }

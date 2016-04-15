2f58a7fa6387d144869f111befe22d880781fba1
Oliver Stadie <hello@waog.net>
2016-04-15 14:46:11 +0200

    ADD Switch and Door

--- Assets/PreparePromoLevelScript.cs
+++ Assets/PreparePromoLevelScript.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class PreparePromoLevelScript : MonoBehaviour {
 
 	public void prepare() {
-		Transform door3 = transform.Find ("level-test/DoorPlaceholder_003");
-		Transform switch1 = transform.Find ("level-test/SwitchPlaceholder_001");
-		switch1.GetComponentInChildren<SwitchScript> ().openDoorScript = door3.GetComponentInChildren<OpenDoorScript> ();
+//		Transform door3 = transform.Find ("level-test/DoorPlaceholder_003");
+//		Transform switch1 = transform.Find ("level-test/SwitchPlaceholder_001");
+//		switch1.GetComponentInChildren<SwitchScript> ().openDoorScript = door3.GetComponentInChildren<OpenDoorScript> ();
 	}
 }

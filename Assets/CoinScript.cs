8a34ab3b1b1a1c00b171b3e84609976300abba9f
thundersphinx <thunder.sphinx@aol.com>
2016-04-29 15:06:22 +0200

    ADD collectable coin

---
+++ Assets/CoinScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinScript : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.tag.Equals(Tags.PLAYER)) {
+			Destroy (transform.parent.gameObject);
+		}
+	}
+}

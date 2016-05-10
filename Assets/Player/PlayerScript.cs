e9caf35a3fca42f5e74e9a864568dd8e9baed95f
Oliver Stadie <hello@waog.net>
2016-05-11 01:32:12 +0200

    UPDATE split gap and bridge, implemented player jump and falling

--- Assets/Player/PlayerScript.cs
+++ Assets/Player/PlayerScript.cs
@@ -5,10 +5,27 @@ public class PlayerScript : MonoBehaviour {
 
 	public Animator gameOverAC;
 
+	bool isFalling = true;
+
+	void Update() {
+		if(Input.GetButton("Jump") && !isFalling) {
+			Vector3 oldVelocity = GetComponent<Rigidbody> ().velocity;
+			GetComponent<Rigidbody> ().velocity = new Vector3 (oldVelocity.x, oldVelocity.y + 5, oldVelocity.z);
+		}
+
+		isFalling = true;
+	}
+
+	void OnCollisionStay(Collision collision) {
+		if (collision.gameObject.layer.Equals (Layers.FLOOR)) {
+			isFalling = false;
+		}
+	}
+
 	void OnCollisionEnter(Collision collision) {
 		if (collision.transform.tag.Equals(Tags.GOAL_SEGMENT)) {
 			gameOverAC.SetTrigger ("Win");
-		} else {
+		} else if (collision.gameObject.layer.Equals(Layers.WALL)) {
 			gameOverAC.SetTrigger ("Lose");
 		}
 	}
--- Assets/Player/WallDetectorScript.cs
+++ Assets/Player/WallDetectorScript.cs
@@ -4,18 +4,22 @@ using System.Collections.Generic;
 
 public class WallDetectorScript : MonoBehaviour {
 
-	IList<Transform> CollidingSegments = new List<Transform>();
+	IList<Transform> CollidingWalls = new List<Transform>();
 
 	public bool isDetectingWall (){
-		return CollidingSegments.Count > 0; 
+		return CollidingWalls.Count > 0; 
 	}
 
 	void OnTriggerEnter(Collider other) {
-		CollidingSegments.Add (other.transform);	
+		if (other.gameObject.layer.Equals (Layers.WALL)) {
+			CollidingWalls.Add (other.transform);	
+		}
 	}
 
 	void OnTriggerExit(Collider other) {
-		CollidingSegments.Remove (other.transform);
+		if (other.gameObject.layer.Equals (Layers.WALL)) {
+			CollidingWalls.Remove (other.transform);
+		}
 	}
 	
 }
